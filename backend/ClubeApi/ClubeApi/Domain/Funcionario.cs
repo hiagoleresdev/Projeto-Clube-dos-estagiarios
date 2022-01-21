5dc4646f280d73b66d5487758eb05cb3855d8bc0
marcotech2002 <marcoantonioporsch@ive.com>
2022-01-21 20:15:35 -0300

    recreating project

--- backend/ClubeApi/ClubeApi/Domain/Funcionario.cs
+++
@@ -1,9 +0,0 @@
-namespace ClubeApi.Domain
-{
-    public class Funcionario : Pessoa
-    {
-        //Declaração de atributos
-        public string Usuario { get; set; }
-        public string Senha { get; set; }
-    }
-}
