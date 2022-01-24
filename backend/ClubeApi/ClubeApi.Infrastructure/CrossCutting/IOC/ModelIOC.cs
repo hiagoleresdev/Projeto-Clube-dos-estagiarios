99ac2a08e26362dddeec3656e6d09bb3aeff6afa
marcotech2002 <marcoantonioporsch@ive.com>
2022-01-24 17:38:12 -0300

    following steps

--- backend/ClubeApi/ClubeApi.Infrastructure/CrossCutting/IOC/ModelIOC.cs
+++ backend/ClubeApi/ClubeApi.Infrastructure/CrossCutting/IOC/ModelIOC.cs
@@ -4,5 +4,9 @@ namespace ClubeApi.Infrastructure.CrossCutting.IOC
 {
     public class ModelIOC : Module
     {
+        protected override void Load(ContainerBuilder builder)
+        {
+            base.Load(builder);
+        }
     }
 }
