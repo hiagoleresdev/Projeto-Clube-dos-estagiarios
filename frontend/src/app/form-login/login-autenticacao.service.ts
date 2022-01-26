d75bfdcd358955b9bdbddb72b7fbd2488b2039e2
pedrotsylos <pedro.sylos@gmail.com>
2022-01-26 17:05:29 -0300

    Login arrumado

--- frontend/src/app/form-login/login-autenticacao.service.ts
+++ frontend/src/app/form-login/login-autenticacao.service.ts
@@ -1,5 +1,5 @@
 import { Usuario } from './usuario';
-import { Injectable } from '@angular/core';
+import { EventEmitter, Injectable } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Injectable({
@@ -9,6 +9,7 @@ export class LoginAutenticacaoService {
 
   private usuarioAutenticado: boolean = false;
 
+  mostrarMenuEmitter = new EventEmitter<boolean>();
 
   constructor(private router: Router) { }
 
@@ -17,9 +18,14 @@ export class LoginAutenticacaoService {
     if(usuario.nome === "usuario@email.com" && usuario.senha === "123456"){
 
       this.usuarioAutenticado = true;
+
+      this.mostrarMenuEmitter.emit(true);
+
       this.router.navigate(["/"]);
     } else {
       this.usuarioAutenticado = false;
+
+      this.mostrarMenuEmitter.emit(false);
     }
   }
 }
