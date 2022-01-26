b98e190f824c1589ccd5ae2456a83a428f6ff30b
LucasRiul <lucasriul85@icloud.com>
2022-01-26 12:46:58 -0300

    first commit

--- backend/ClubeApi/ClubeApi/obj/Release/net6.0/ClubeApi.AssemblyInfo.cs
+++ backend/ClubeApi/ClubeApi/obj/Release/net6.0/ClubeApi.AssemblyInfo.cs
@@ -11,8 +11,13 @@
 using System;
 using System.Reflection;
 
-[assembly: Microsoft.AspNetCore.Mvc.ApplicationParts.ApplicationPartAttribute("Swashbuckle.AspNetCore.Annotations")]
-[assembly: Microsoft.AspNetCore.Mvc.ApplicationParts.ApplicationPartAttribute("Swashbuckle.AspNetCore.SwaggerGen")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("ClubeApi")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
+[assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyProductAttribute("ClubeApi")]
+[assembly: System.Reflection.AssemblyTitleAttribute("ClubeApi")]
+[assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Gerado pela classe WriteCodeFragment do MSBuild.
 
